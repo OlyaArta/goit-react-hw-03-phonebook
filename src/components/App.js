ff3b2c386dfdf02cc1d35d5d525757b706d85fea
OlyaArta <artanika8@gmail.com>
2021-10-14 23:31:05 +0300

    add logic of local storage

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ import Filter from "./filter/Filter";
 class App extends React.Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
@@ -55,6 +55,23 @@ class App extends React.Component {
       contacts: prevState.contacts.filter((contact) => contact.id !== id),
     }));
   };
+
+  componentDidMount() {
+    console.log("something");
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    console.log(parsedContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log("tada");
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
